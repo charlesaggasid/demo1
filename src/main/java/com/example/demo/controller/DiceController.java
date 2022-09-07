59ce4bbdac97ee884c879bc95aa530242281a9f5
Charles Aggasid <charles.aggasid01@gmail.com>
2022-09-06 22:05:16 -0500

    done roll-dice

--- src/main/java/com/example/demo/controller/DiceController.java
+++ src/main/java/com/example/demo/controller/DiceController.java
@@ -1,11 +1,27 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class DiceController {
 
     @GetMapping("/roll-dice")
-    public String showOptions
+    public String showOptions() {
+        return "roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{n}")
+    public String showResult(@PathVariable int n, Model model) {
+        int randomNum = (int) (Math.random() * 6) + 1; //generate random number to compare
+
+        if (n == randomNum) {
+            model.addAttribute("result", "You guessed correctly");
+        } else {
+            model.addAttribute("result", "Sorry, you guess was incorrect");
+        }
+        return "roll-dice";
+    }
 }
